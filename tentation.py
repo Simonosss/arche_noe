8267f980bb48c393efe07d45f07f232785cbc786
hackerdu80 <164486249+hackerdu80@users.noreply.github.com>
2024-03-22 10:48:04 +0100

    truc qui marche

--- tentation.py
+++ tentation.py
@@ -1,131 +1,167 @@
 
 
-class Animaux:
-    def __init__ (self):
-        self.lion=1
-        self.lionne= 2
-        self.giraffe=3
-        self.girafe=4
-        self.hypoM=5
-        self.hypoF=6
-        self.elephant=7
-        self.elephante=8
-
 import pygame
+from PIL import Image, ImageFilter
 import sys
 
-# Initialisation de Pygame
+
+class Jeu :
+    def __init__ (self,nom,mdp):
+        self.nom=nom
+        self.mdp=mdp
+    def enregistrer(self):
+        self.dic={}
+        self.dic[self.nom]=self.mdp
+    def login (self):
+        if self.nom in self.dic:
+            mdp=0
+            self.dic[self.nom]=mdp
+            if self.mdp==mdp :
+                return True
+            else :
+                return False
+        else :
+            return False
+    def fenetre (self):
+        largeur = 1920
+        hauteur = 1080
+        taille_fenetre = (largeur, hauteur)
+
+        
+        fenetre = pygame.display.set_mode(taille_fenetre)
+        pygame.display.set_caption("Arche_02.jpeg")
+
+        
+        image_arche = pygame.image.load("Arche_02.jpeg")
+        image_lion = pygame.image.load("SGT 240-B_0.jpg")  
+        image_chien = pygame.image.load("SGT 240-H_0.jpg")  
+        image_chat = pygame.image.load("SGT 240-G_0.jpg")    
+        
+
+        
+        position_lion = (300, 300)
+        position_chien = (100, 200)
+        position_chat = (200, 300)
+        
+
+        animal_selectionne = False
+        animal_en_cours = None
+        
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if image_chien.get_rect(topleft=position_chien).collidepoint(event.pos):
+                        animal_selectionne = True
+                        animal_en_cours = 'chien'
+                    elif image_chat.get_rect(topleft=position_chat).collidepoint(event.pos):
+                        animal_selectionne = True
+                        animal_en_cours = 'chat'
+                    elif image_lion.get_rect(topleft=position_lion).collidepoint(event.pos):
+                        animal_selectionne = True
+                        animal_en_cours ='lion'
+
+                        
+                    
+
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    animal_selectionne = False
+
+                
+                if animal_selectionne and event.type == pygame.MOUSEMOTION:
+                    if animal_en_cours == 'chien':
+                        position_chien = event.pos
+                    elif animal_en_cours == 'chat':
+                        position_chat = event.pos
+                    elif animal_en_cours == 'lion':
+                        position_lion = event.pos
+                        
+            
+            fenetre.fill((255, 255, 255))  
+
+            
+            fenetre.blit(image_arche, (0, 0))
+
+            
+            fenetre.blit(image_chien, position_chien)
+            fenetre.blit(image_chat, position_chat)
+            fenetre.blit(image_lion, position_lion)
+            
+            pygame.display.flip()
+            
+        pygame.quit()
+        
+        
+
+
 pygame.init()
 
-# Définir la taille de la fenêtre
+
 largeur = 1920
 hauteur = 1080
 taille_fenetre = (largeur, hauteur)
 
-# Création de la fenêtre
+image_jeu = pygame.image.load("Arche_02.jpeg")  
+
+pil_image = Image.open("Arche_02.jpeg")  
+pil_image = pil_image.filter(ImageFilter.GaussianBlur(radius=10))  
+
+image_floue = pygame.image.fromstring(pil_image.tobytes(), pil_image.size, pil_image.mode)
+
 fenetre = pygame.display.set_mode(taille_fenetre)
-pygame.display.set_caption("Arche_02.jpeg")
+pygame.display.set_caption("Menu d'Accueil")
 
-# Charger les images des animaux
-image_arche = pygame.image.load("Arche_02.jpeg")
-image_lion = pygame.image.load("SGT 240-B_0.jpg")  # Image de l'arche
-image_chien = pygame.image.load("SGT 240-H_0.jpg")  # Image du chien
-image_chat = pygame.image.load("SGT 240-G_0.jpg")    # Image du chat
-# Chargez les images des autres animaux de la même manière...
+font = pygame.font.Font(None, 64)
+titre_text = font.render("Bienvenue au jeu", True, (255, 255, 255))
 
-# Position initiale des animaux
-position_lion = (300, 300)
-position_chien = (100, 200)
-position_chat = (200, 300)
-# Définissez les positions initiales des autres animaux de la même manière...
+titre_rect = titre_text.get_rect(center=(largeur // 2, 100))
 
-animal_selectionne = False
-animal_en_cours = None
+bouton_jouer_rect = pygame.Rect(largeur // 2 - 100, 300, 200, 50)
+bouton_quitter_rect = pygame.Rect(largeur // 2 - 100, 400, 200, 50)
 
-# Écran d'accueil
-def ecran_accueil():
-    fenetre.fill((255, 255, 255))  # Remplir l'écran avec une couleur blanche
+blanc = (255, 255, 255)
+noir = (0, 0, 0)
 
-    # Texte d'accueil
-    font = pygame.font.Font(None, 64)
-    jouer_text = font.render("Jouer", True, (0, 0, 0))
-    quitter_text = font.render("Quitter", True, (0, 0, 0))
-
-    # Positionnement des textes
-    jouer_rect = jouer_text.get_rect(center=(largeur // 2, hauteur // 2 - 50))
-    quitter_rect = quitter_text.get_rect(center=(largeur // 2, hauteur // 2 + 50))
-
-    # Dessin des textes sur l'écran
-    fenetre.blit(jouer_text, jouer_rect)
-    fenetre.blit(quitter_text, quitter_rect)
-
-    pygame.display.flip()
-
-# Boucle pour l'écran d'accueil
-ecran_accueil()
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if jouer_rect.collidepoint(event.pos):
-                running = True
-                break
-            elif quitter_rect.collidepoint(event.pos):
-                pygame.quit()
-                sys.exit()
-    pygame.display.update()
-
-# Boucle principale
+partie=Jeu('marcel', 'lol')
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if bouton_jouer_rect.collidepoint(event.pos):
+                partie.fenetre()
+            elif bouton_quitter_rect.collidepoint(event.pos):
+                pygame.quit()
+                sys.exit()
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            # Vérifiez si un animal est cliqué
-            if image_chien.get_rect(topleft=position_chien).collidepoint(event.pos):
-                animal_selectionne = True
-                animal_en_cours = 'chien'
-            elif image_chat.get_rect(topleft=position_chat).collidepoint(event.pos):
-                animal_selectionne = True
-                animal_en_cours = 'chat'
-            elif image_lion.get_rect(topleft=position_lion).collidepoint(event.pos):
-                animal_selectionne = True
-                animal_en_cours ='lion'
+    # Afficher l'image floue en arrière-plan
+    fenetre.blit(image_floue, (0, 0))
+    
+    # Dessiner le titre
+    fenetre.blit(titre_text, titre_rect)
 
-                
-            # Ajoutez les autres animaux de la même manière...
+    # Dessiner les boutons
+    pygame.draw.rect(fenetre, noir, bouton_jouer_rect)
+    pygame.draw.rect(fenetre, noir, bouton_quitter_rect)
 
-        elif event.type == pygame.MOUSEBUTTONUP:
-            animal_selectionne = False
+    # Texte des boutons
+    font_bouton = pygame.font.Font(None, 32)
+    jouer_text = font_bouton.render("Jouer", True, blanc)
+    quitter_text = font_bouton.render("Quitter", True, blanc)
 
-        # Mettre à jour la position de l'animal sélectionné
-        if animal_selectionne and event.type == pygame.MOUSEMOTION:
-            if animal_en_cours == 'chien':
-                position_chien = event.pos
-            elif animal_en_cours == 'chat':
-                position_chat = event.pos
-            elif animal_en_cours == 'lion':
-                position_lion = event.pos
-                
-    # Effacer l'écran
-    fenetre.fill((255, 255, 255))  # Remplir l'écran avec une couleur blanche
+    # Positionnement des textes des boutons
+    jouer_rect = jouer_text.get_rect(center=bouton_jouer_rect.center)
+    quitter_rect = quitter_text.get_rect(center=bouton_quitter_rect.center)
 
-    # Dessiner l'arche
-    fenetre.blit(image_arche, (0, 0))
+    # Affichage des textes des boutons
+    fenetre.blit(jouer_text, jouer_rect)
+    fenetre.blit(quitter_text, quitter_rect)
 
-    # Dessiner les animaux
-    fenetre.blit(image_chien, position_chien)
-    fenetre.blit(image_chat, position_chat)
-    fenetre.blit(image_lion, position_lion)
-    # Dessinez les autres animaux de la même manière...
-
-    # Mise à jour de l'affichage
     pygame.display.flip()
 
-# Quitter Pygame
 pygame.quit()
 sys.exit()
